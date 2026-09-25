69aaf8d178a14847d564fc996da0b9704815ecfe
KCY <chienyoungkho@gmail.com>
2026-09-25 11:44:34 +0800

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hello, world")
+
+function getComputerChoice() {
+    let randomNumber = Math.random();
+
+    if(randomNumber < (1/3)){
+        return "rock";
+    } else if( randomNumber < (2/3)){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice())
